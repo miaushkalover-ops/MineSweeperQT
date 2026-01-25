70f7232bc2cce0b9360fbbaa009a75ea9ff681fe
MiaushkaLover <miaushkalover@gmail.com>
2026-01-25 18:35:33 -0500

    _

--- src/scripts/game.py
+++ src/scripts/game.py
@@ -11,13 +11,13 @@ from scripts.styles import Styles
 
 
 class Mine_Sweeper(QMainWindow):
-    def __init__(self, rows_: int, cols_: int, mines_: int) -> None:
+    def __init__(self, rows_, cols_, mines_) -> None:
         super().__init__()
 
         self.sounds = SoundManager()
 
         self.rows = rows_
-        self.cols = cols_
+        self.columns = cols_
         self.mines = mines_
 
         self.counter = 0
@@ -32,18 +32,18 @@ class Mine_Sweeper(QMainWindow):
         widget = QWidget()
         main_layout = QGridLayout()
 
-        self.setFixedSize(QSize(self.cols * 40, self.rows * 40))
+        self.setFixedSize(QSize(self.columns * 40, self.rows * 40))
         self.setWindowTitle('MineSweeper QT')
 
         self.field: list[list[Cell]] = [
-            [Cell(is_barrier=True) for _ in range(self.cols + 2)],
-            *[[Cell(is_barrier=True), *[Cell() for _ in range(self.cols)],
+            [Cell(is_barrier=True) for _ in range(self.columns + 2)],
+            *[[Cell(is_barrier=True), *[Cell() for _ in range(self.columns)],
                 Cell(is_barrier=True)] for _ in range(self.rows)],
-            [Cell(is_barrier=True) for _ in range(self.cols + 2)],
+            [Cell(is_barrier=True) for _ in range(self.columns + 2)],
         ]
 
         for i in range(1, self.rows + 1):
-            for j in range(1, self.cols + 1):
+            for j in range(1, self.columns + 1):
                 main_layout.addWidget(self.field[i][j], i, j)
                 self.field[i][j].position = (i, j)
 
@@ -57,9 +57,9 @@ class Mine_Sweeper(QMainWindow):
         self.game_active = True
 
         for i in range(1, self.rows + 1):
-            for j in range(1, self.cols + 1):
+            for j in range(1, self.columns + 1):
                 c = self.field[i][j]
-                c.is_open = False
+                c.is_mine = False
                 c.is_open = False
                 c.value = '0'
                 c.setText('')
@@ -68,7 +68,7 @@ class Mine_Sweeper(QMainWindow):
 
     def connect_signals(self) -> None:
         for i in range(1, self.rows + 1):
-            for j in range(1, self.cols + 1):
+            for j in range(1, self.columns + 1):
                 cell = self.field[i][j]
                 cell.game_over_signal.connect(self.show_game_over_dialog)
                 cell.run_bfs_signal.connect(self.breadth_first_search)
@@ -78,7 +78,7 @@ class Mine_Sweeper(QMainWindow):
     def on_left_click(self, pos: tuple[int, int]) -> None:
         if not self.game_active:
             return
-        
+
         if self.first_click:
             self.first_click = False
             self.place_mines_avoiding(pos)
@@ -92,23 +92,23 @@ class Mine_Sweeper(QMainWindow):
         for di in (-1, 0, 1):
             for dj in (-1, 0, 1):
                 ni, nj = ci + di, cj + dj
-                if 1 <= ni <= self.rows and 1 <= nj <= self.cols:
+                if 1 <= ni <= self.rows and 1 <= nj <= self.columns:
                     forbidden.add((ni, nj))
 
         mines_set = set()
         while len(mines_set) < self.mines:
-            m = (randint(1, self.rows), randint(1, self.cols))
+            m = (randint(1, self.rows), randint(1, self.columns))
             if m in forbidden:
                 continue
             mines_set.add(m)
 
-        for mi, mj in  mines_set:
+        for mi, mj in mines_set:
             self.field[mi][mj].is_mine = True
             self.field[mi][mj].value = '*'
 
     def compute_numbers(self) -> None:
         for i in range(1, self.rows + 1):
-            for j in range(1, self.cols + 1):
+            for j in range(1, self.columns + 1):
                 if self.field[i][j].is_mine:
                     continue
                 self.field[i][j].value = str([
@@ -121,17 +121,17 @@ class Mine_Sweeper(QMainWindow):
     def on_cell_opened(self, cell: Cell) -> None:
         if not self.game_active:
             return
-        
+
         self.counter += 1
-        if self.counter == self.rows * self.cols - self.mines:
+        if self.counter == self.rows * self.columns - self.mines:
             self.show_winner_dialog()
 
-    @dialog_decorator(title='You Lost !')
+    @dialog_decorator(title='You lost !')
     def show_game_over_dialog(self) -> None:
         self.game_active = False
         self.reveal_full_field()
 
-    @dialog_decorator(title='You Won !')
+    @dialog_decorator(title='You won !')
     def show_winner_dialog(self) -> None:
         self.game_active = False
 
@@ -141,7 +141,7 @@ class Mine_Sweeper(QMainWindow):
     def breadth_first_search(self, position: tuple[int, int]) -> None:
         if not self.game_active:
             return
-        
+
         q = deque([position])
         visited = {position}
 
@@ -162,7 +162,7 @@ class Mine_Sweeper(QMainWindow):
                     if di == 0 and dj == 0:
                         continue
                     ni, nj = i + di, j + dj
-                    if not (1 <= ni <= self.rows and 1 <= nj <= self.cols):
+                    if not (1 <= ni <= self.rows and 1 <= nj <= self.columns):
                         continue
                     if (ni, nj) in visited:
                         continue
@@ -171,12 +171,10 @@ class Mine_Sweeper(QMainWindow):
                     if neighbor.is_mine or neighbor.is_barrier:
                         continue
 
-                    visited.add((ni, ni))
+                    visited.add((ni, nj))
                     q.append((ni, nj))
 
     def reveal_full_field(self) -> None:
         for i in range(1, self.rows + 1):
-            for j in range(1, self.cols + 1):
+            for j in range(1, self.columns + 1):
                 self.field[i][j].reveal()
-
-                    
